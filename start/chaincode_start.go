31b87c8cff46a6a58d14a2cbee26164ccbb6731f
Nagmani15 <nagmani.a.kumar@capgemini.com>
2017-02-23 18:50:43 +0530

    client code added

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -91,7 +91,9 @@ func (t *SimpleChaincode) checkBalance(stub shim.ChaincodeStubInterface,args []s
 //transfer money
 func (t *SimpleChaincode) sendMoney(stub shim.ChaincodeStubInterface,args []string) (int  , error) {
 amount, err := stub.GetState("Initial_Amount");
- err: = stub.PutState("Initial_Amount", (amount));
+	var amt
+	amt := amount-strconv.ParseInt(args[0]);
+ //err: = stub.PutState("Initial_Amount", amt);
 
 	if err != nil { fmt.Printf("SAVE_CHANGES: Error storing payment record: %s", err); return nil, errors.New("Error storing payment record") }
 	return nil, errors.New("############Received unknown function query: "+string(amount[:]))
